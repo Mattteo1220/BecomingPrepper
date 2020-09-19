c6dfccb8a0fdb5e58a4a3b9beddd35327cd47d62
Matthew McEntire <mmcentire@healthequity.com>
2020-09-19 16:00:15 -0600

    introduced interface for servicing account.

---
+++ BecomingPrepper.Core/UserUtility/Interfaces/IServiceAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace BecomingPrepper.Core.UserUtility.Interfaces
+{
+    public interface IServiceAccount
+    {
+        void UpdatePassword(string password);
+        void UpdateFamilySize(int familySize);
+        void UpdateObjective(int objective);
+        void UpdateEmail(string email);
+    }
+}
