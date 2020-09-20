54d185d2b3bc6bc045241ffe27a458ecaab4df50
Matthew McEntire <mmcentire@healthequity.com>
2020-09-19 21:05:22 -0600

    added in integration test for update email

---
+++ BecomingPrepper.Tests/IntegrationTests/ServiceAccountTests/UpdateEmail/UpdateEmail.feature
@@ -0,0 +1,11 @@
+﻿Feature: UpdateEmail
+	In order for users to update their email
+	They can update it within their account
+	So that they can receive emails
+
+@UserRepository @DisposeUser
+Scenario: Update Email
+	Given A User
+	And That user is registered
+	When That user updates their email
+	Then it is saved to the database
---
+++ BecomingPrepper.Tests/IntegrationTests/ServiceAccountTests/UpdateEmail/UpdateEmailSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AutoFixture;
+using BecomingPrepper.Data.Entities;
+using FluentAssertions;
+using MongoDB.Driver;
+using TechTalk.SpecFlow;
+
+namespace BecomingPrepper.Tests.IntegrationTests.ServiceAccountTests.UpdateEmail
+{
+    [Binding]
+    public class UpdateEmailSteps
+    {
+        private readonly UserContext _userContext;
+
+        public UpdateEmailSteps(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        [When(@"That user updates their email")]
+        public void WhenThatUserUpdatesTheirEmail()
+        {
+            _userContext.PropertyUpdate = new Fixture().Create<string>();
+            var filter = Builders<UserEntity>.Filter.Eq(u => u.AccountId, _userContext.UserEntity.AccountId);
+            var update = Builders<UserEntity>.Update.Set(u => u.Account.Email, _userContext.PropertyUpdate);
+
+            _userContext.UserRepository.Update(filter, update);
+        }
+
+        [Then(@"it is saved to the database")]
+        public void ThenItIsSavedToTheDatabase()
+        {
+            var filter = Builders<UserEntity>.Filter.Eq(u => u.AccountId, _userContext.UserEntity.AccountId);
+            TestHelper.WaitUntil(() => _userContext.UserRepository.Get(filter) != null, TimeSpan.FromMilliseconds(30000));
+            _userContext.UserRepository.Get(filter).Account.Email.Should().BeEquivalentTo(_userContext.PropertyUpdate, "Email was updated.");
+        }
+    }
+}
